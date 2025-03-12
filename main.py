84c1290280f6cc3f14b30d8a23d9c0ec837e0c20
Sunpheus_ <118692310+Sunpheusgithub@users.noreply.github.com>
2025-03-12 08:46:29 +0000

    données

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import random
+structure = input('type de poeme ex:sonnet') #string
+syllabes = input('nombre de syllabes') #integer
+type_rime = input('type de rimes ex:ABAB') #list
+rimes = input('phonétique du poeme forme : "ou","é"') #tuple
+theme = input ('quelle est la thematique du poeme') #string
+
