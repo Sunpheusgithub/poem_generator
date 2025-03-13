a9a4070306b6c067e480af8043edb9979133ad74
riwal-847 <riwal.caroff@gmail.com>
2025-03-13 21:39:58 +0100

    Update gael version du code.py

--- gael ver du code.py
+++ gael ver du code.py
@@ -46,7 +46,9 @@ def ecrire_le_vers(syllabes:str,theme:str,rimes:str) -> str:
                    "Vous":["mangez","parlez","sentez","touchez","ressentez","rêvez","tapez"],
                    "Ils":["mangent","parlent","sentent","touchent","ressentent","rêvent","tapent"],
                    "Elles":["mangent","parlent","sentent","touchent","ressentent","rêvent","tapent"]}
-    determinant_liste_singulier = ["le","la","me","ma","un","une"]
+    #ON POURRA RAJOUTER DES DETERMINANTS POSSESSIFS APRES
+    determinant_liste_singulier_masculin = ["le","une"] 
+    determinant_liste_singulier_feminin = ["la","une"]
     determinant_liste_pluriel = ["des","tes","les","mes"]
     sujet_nature = {"singulier":{"1":["lion","lionne","fleur","feuille","vent","mer","roche","champs","pluie"],
                            "2":["amie","ami","soleil","nuage","banane","chemin","orage","bouleau","mangrove"],
@@ -69,15 +71,21 @@ def ecrire_le_vers(syllabes:str,theme:str,rimes:str) -> str:
     #ON CHOISIT UN VERBE ALEATOIRE, EN LIEN AVEC LE SUJET
     verbe = random.choice(verbe_liste[sujet_a_utiliser])
     vers = vers+" "+verbe
-    #ON CHOISIT SI LA SUITE VA ETRE AU SINGULIER OU AU PLURIEL
+    #ON CHOISIT SI LA SUITE VA ETRE AU SINGULIER OU AU PLURIEL ET FEMININ OU MASCULIN
     sing_ou_plur = random.choice(["singulier","pluriel"])
-    determinant = random.choice(determinant_liste_singulier)
+    femin_ou_masc = random.choice(["masculin","feminin"])
+    determinant = random.choice(determinant_liste_singulier_masculin)
+    if femin_ou_masc == "feminin":
+        determinant = random.choice(determinant_liste_singulier_feminin)
     if sing_ou_plur == "pluriel":
         determinant = random.choice(determinant_liste_pluriel)
-    vers = vers+" "+determinant
     #ON CHOISIT UN NOM ADAPTE AU DETERMINANT ET AU THEME
-    nom = choisir_nom(theme,sing_ou_plur)
-    vers = vers+" "+nom
+    nom = choisir_nom(theme,sing_ou_plur,femin_ou_masc)
+    #ON REMPLACE LE DETERMINANT PAR UN L APOSTROPHE SI LE NOM COMMENCE PAR UNE VOYELLE
+    vers = vers+" "+determinant+" "+nom
+    if nom[0] in ["a","e","y","u","i","o"] and determinant in ["la","le"]:
+        determinant = "l'"
+        vers = sujet+" "+verbe+" "+determinant+""+nom
     return vers
 
 def prenom_vers_il(sujet:str,sujet_liste_prenom:list) -> str:
@@ -86,18 +94,23 @@ def prenom_vers_il(sujet:str,sujet_liste_prenom:list) -> str:
         sujet_retourne = "Il"
     return sujet_retourne
 
-def choisir_nom(theme:str,sing_ou_plur:str) -> str:
+def choisir_nom(theme:str,sing_ou_plur:str,femin_ou_masc:str) -> str:
     nom = ""
-    sujet_nature = {"singulier":{"1":["lion","lionne","fleur","feuille","vent","mer","roche","champs","pluie"],
-                           "2":["amie","ami","soleil","nuage","banane","chemin","orage","bouleau","mangrove"],
-                           "3":["papillon","arbuste","océanique","tournesol","écureuil","chèvrefeuille","canopée"]},
+    sujet_nature = {"singulier":{"1":{"masculin":["lion","vent","champs"],"feminin":["lionne","fleur","feuille","mer","roche","champs","pluie"]},
+                           "2":{"masculin":["ami","soleil","nuage","chemin","orage","bouleau"],"feminin":["amie","banane","mangrove"]},
+                           "3":{"masculin":["papillon","arbuste","abyssal","tournesol","écureuil","chèvrefeuille"],"feminin":["canopée","léoparde"]}},
               "pluriel":{"1":["lions","lionnes","fleurs","feuilles","vents","mers","roches","champs","pluies"],
                            "2":["amies","amis","soleils","nuages","bananes","chemins","orages","bouleaux","mangroves"],
                            "3":["papillons","arbustes","océaniques","tournesols","écureuils","chèvrefeuilles","canopées"]}}
     if theme == "nature":
-        nom = random.choice(sujet_nature["singulier"][random.choice(["1","2","3"])])
+        if femin_ou_masc == "masculin":
+            nom = random.choice(sujet_nature["singulier"][random.choice(["1","2","3"])]["masculin"])
+        if femin_ou_masc == "feminin":
+            nom = random.choice(sujet_nature["singulier"][random.choice(["1","2","3"])]["feminin"])
         if sing_ou_plur == "pluriel":
             nom = random.choice(sujet_nature["pluriel"][random.choice(["1","2","3"])])
+            femin_ou_masc = "pluriel"
+    print(femin_ou_masc)
     return nom
 
 def afficher_poeme(poeme:list):
