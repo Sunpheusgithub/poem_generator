fbf8ff293d9a607308f876c1db382cbae93f304d
Sunpheus_ <118692310+Sunpheusgithub@users.noreply.github.com>
2025-03-27 10:52:47 +0000

    Enhance verb and noun lists to support additional phonetic categories and improve data structure

--- dico_gen.py
+++ dico_gen.py
@@ -6,24 +6,23 @@ verbes_liste = {
     "On": [],
     "Nous": [],
     "Vous": [],
-    "Ils": [],  # Partagé avec Elles
-    "Elles": []  # Partagé avec Ils
+    "Ils": [],  
+    "Elles": [] 
 }
 
-# Charger les données du fichier
 with open("dico_verbes.txt", "r", encoding="utf-8") as fichier:
     for ligne in fichier:
         mots = ligne.strip().split(", ")
-        if len(mots) == 7:  # Vérifie que la ligne contient 7 colonnes
-            verbes_liste["Je"].append(mots[1])  # 1ère personne du singulier
-            verbes_liste["Tu"].append(mots[2])  # 2ème personne du singulier
-            verbes_liste["Il"].append(mots[3])  # 3ème personne du singulier (masculin)
-            verbes_liste["Elle"].append(mots[3])  # 3ème personne du singulier (féminin)
-            verbes_liste["On"].append(mots[3])  # 3ème personne du singulier (neutre)
-            verbes_liste["Nous"].append(mots[4])  # 1ère personne du pluriel
-            verbes_liste["Vous"].append(mots[5])  # 2ème personne du pluriel
-            verbes_liste["Ils"].append(mots[6])  # 3ème personne du pluriel (partagé)
-            verbes_liste["Elles"].append(mots[6])  # 3ème personne du pluriel (partagé)
+        if len(mots) == 7:
+            verbes_liste["Je"].append(mots[1]) 
+            verbes_liste["Tu"].append(mots[2])  
+            verbes_liste["Il"].append(mots[3])  
+            verbes_liste["Elle"].append(mots[3])  
+            verbes_liste["On"].append(mots[3])  
+            verbes_liste["Nous"].append(mots[4])  
+            verbes_liste["Vous"].append(mots[5]) 
+            verbes_liste["Ils"].append(mots[6])  
+            verbes_liste["Elles"].append(mots[6])  
 
 print(verbes_liste)
 
@@ -31,15 +30,24 @@ noms_liste = {
     "masculin": [],
     "feminin": [],
     "pMasculin": [],
-    "pFeminin": []
+    "pFeminin": [],
+    "phonetique_masculin": [],
+    "phonetique_feminin": [],
+    "phonetique_pMasculin": [],
+    "phonetique_pFeminin": []
 }
 
-# Charger les données du fichier
 with open("dico_nCommuns.txt", "r", encoding="utf-8") as fichier:
     for ligne in fichier:
         mots = ligne.strip().split(", ")
-        if len(mots) == 4:  # Vérifie que la ligne contient 4 colonnes
+        if len(mots) == 8:  
             noms_liste["masculin"].append(mots[0])
             noms_liste["feminin"].append(mots[1])
             noms_liste["pMasculin"].append(mots[2])
             noms_liste["pFeminin"].append(mots[3])
+            noms_liste["phonetique_masculin"].append(mots[4])
+            noms_liste["phonetique_feminin"].append(mots[5])
+            noms_liste["phonetique_pMasculin"].append(mots[6])
+            noms_liste["phonetique_pFeminin"].append(mots[7])
+
+print(noms_liste)
